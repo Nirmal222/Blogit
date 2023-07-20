30b83ace261d376c660c53438d32898a36909c97
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-07-20 23:55:34 +0530

    Added /send-otp route

--- backend/src/utils/nodemailer/transporter.js.js
+++ backend/src/utils/nodemailer/transporter.js.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
-
+// dotenv 
+require('dotenv').config();
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-email-password',
+      user: 'nirmalkumargurajada@gmail.com',
+      pass: process.env.EMAIL_PASSWORD,
     },
 });
 
